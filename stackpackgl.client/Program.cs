432a11465b8c568b7bf35dff988badb078d221b3
Kyle Treleaven <ktreleaven@symbotic.com>
2015-08-09 20:28:38 -0400

    first template filled by mono project

--- stackpackgl.client/Program.cs
+++ stackpackgl.client/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using stackpackgl.report;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace stackpackgl.client
 {
@@ -8,7 +9,31 @@ namespace stackpackgl.client
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine (new HashSet<IBox2> ().ToThreeJs ());
+			int n = 10;
+			float width = 20, gap = 5, totalWidth = n * width + (n - 1) * gap;
+
+			IEnumerable<IBox3> boxes
+				= Enumerable.Range(0,n)
+					.SelectMany( i => 
+				Enumerable.Range(0,n)
+					.SelectMany( j =>
+						Enumerable.Range(0,n)
+						.Select( k => new {i=i,j=j,k=k} ) ) )
+				.Select (s => new
+					{
+						x = -.5f * totalWidth + s.i * (width + gap),
+						y = -.5f * totalWidth + s.j * (width + gap),
+						z = -.5f * totalWidth + s.k * (width + gap)
+					})
+				.Select (s => (IBox3) new Box3 () {
+				Xmin = s.x, Xmax = s.x + width,
+				Ymin = s.y, Ymax = s.y + width,
+				Zmin = s.z, Zmax = s.z + width
+			});
+			
+			Console.WriteLine (boxes.ToThreeJs());
+
+			//Console.ReadKey ();
 		}
 	}
 }
--- stackpackgl.report/StackPackToThreeJs.cs
+++ stackpackgl.report/StackPackToThreeJs.cs
@@ -1,30 +1,51 @@
 using System;
 using System.Collections.Generic;
+using HtmlAgilityPack;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace stackpackgl.report
 {
-	public interface IBox2
+	public interface IBox3
 	{
 		float Xmin {get;}
 		float Xmax {get;}
 		float Ymin {get;}
 		float Ymax {get;}
+		float Zmin {get;}
+		float Zmax { get; }
+
 	}
 
-	public struct Box2 : IBox2
+	public struct Box3 : IBox3
 	{
 		public float Xmin {get;set;}
 		public float Xmax {get;set;}
 		public float Ymin {get;set;}
 		public float Ymax {get;set;}
+		public float Zmin {get;set;}
+		public float Zmax {get;set;}
 	}
 
 	public static class StackPackToThreeJs
 	{
-		public static string ToThreeJs(this ICollection<IBox2> boxes)
+		public static string ToThreeJs(this IEnumerable<IBox3> boxes)
 		{
-			return "Hello World!";
+			string jsonString;
+
+			var boxData = boxes.Select (box => new float [] { box.Xmin, box.Xmax, box.Ymin, box.Ymax, box.Zmin, box.Zmax });
+			jsonString = JsonConvert.SerializeObject(boxData);
+
+			HtmlWeb htmlWeb = new HtmlWeb();
+
+			HtmlDocument htmlDocument = htmlWeb.Load ("C:\\Users\\ktreleaven\\sandbox\\_prototypes\\stackpackgl\\stackpackgl.report\\template.html");
+		
+			var dataNode = htmlDocument.GetElementbyId ("boxdata");
+
+			dataNode.InnerHtml = jsonString;
+
+			return htmlDocument.DocumentNode.OuterHtml;
 		}
 	}
 }
-
